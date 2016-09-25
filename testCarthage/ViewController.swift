5d8b5dc0d8309d1d7a9d5e43fcba43827bb8890a
Behnam Hajian <behnam@ca.ibm.com>
2016-09-25 11:12:48 +0200

    Initial Commit

---
+++ testCarthage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testCarthage
+//
+//  Created by behnam hajian on 2016-09-25.
+//  Copyright © 2016 bluekernel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
